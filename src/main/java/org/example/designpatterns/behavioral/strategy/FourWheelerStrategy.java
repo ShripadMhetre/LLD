25bb10c4967237299c24d4107ef623c8dabd91b0
ShripadMhetre <shripadmhetre100@gmail.com>
2023-10-10 22:37:59 +0530

    add: strategy pattern

---
+++ src/main/java/org/example/designpatterns/behavioral/strategy/FourWheelerStrategy.java
@@ -0,0 +1,9 @@
+package org.example.designpatterns.behavioral.strategy;
+
+// Step 2: Creating concrete Strategies
+public class FourWheelerStrategy implements NavigationStrategy {
+    @Override
+    public void navigate(String from, String to) {
+        System.out.println("Navigation for Four Wheeler Vehicle....");
+    }
+}
